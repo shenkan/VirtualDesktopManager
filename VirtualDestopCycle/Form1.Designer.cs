2484cbcbd49fa0bdc92f0d8509fc30e2e170e552
Joe Ipson <joeipson@gmail.com>
2016-03-15 10:05:59 -0600

    Initial commit

---
+++ VirtualDestopCycle/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace VirtualDesktopManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
+            this.notifyIcon1.Icon = Properties.Resources.mainIcon;
+            this.notifyIcon1.Text = "Virtual Desktop Cycler";
+            this.notifyIcon1.Visible = true;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(28, 28);
+            this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(120, 38);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(119, 34);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(300, 126);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Virtual Desktop Cycler";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+
+
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
---
+++ VirtualDestopCycle/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Runtime.InteropServices;
+using WindowsDesktop;
+using GlobalHotKey;
+
+namespace VirtualDesktopManager
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", ExactSpelling = true)]
+        static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private IList<VirtualDesktop> desktops = VirtualDesktop.GetDesktops();
+        private IntPtr[] activePrograms;
+
+        private readonly HotKeyManager _rightHotkey;
+        private readonly HotKeyManager _leftHotkey;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            activePrograms = new IntPtr[desktops.Count];
+
+            _rightHotkey = new HotKeyManager();
+            _rightHotkey.KeyPressed += RightKeyManagerPressed;
+
+            _leftHotkey = new HotKeyManager();
+            _leftHotkey.KeyPressed += LeftKeyManagerPressed;
+
+            VirtualDesktop.CurrentChanged += VirtualDesktop_CurrentChanged;
+        }
+
+        private void VirtualDesktop_CurrentChanged(object sender, VirtualDesktopChangedEventArgs e)
+        {
+            int currentDesktopIndex = getCurrentDesktopIndex();
+
+            restoreApplicationFocus(currentDesktopIndex);
+            changeTrayIcon(currentDesktopIndex);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            _rightHotkey.Dispose();
+            _leftHotkey.Dispose();
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _rightHotkey.Register(Key.Right, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+            _leftHotkey.Register(Key.Left, System.Windows.Input.ModifierKeys.Control | System.Windows.Input.ModifierKeys.Alt);
+
+            var desktop = initialDesktopState();
+            changeTrayIcon();
+        }
+
+        private int getCurrentDesktopIndex()
+        {
+            return desktops.IndexOf(VirtualDesktop.Current);
+        }
+
+        private void saveApplicationFocus(int currentDesktopIndex = -1)
+        {
+            IntPtr activeAppWindow = GetForegroundWindow();
+
+            if (currentDesktopIndex == -1)
+                currentDesktopIndex = getCurrentDesktopIndex();
+
+            activePrograms[currentDesktopIndex] = activeAppWindow;
+        }
+
+        private void restoreApplicationFocus(int currentDesktopIndex = -1)
+        {
+            if (currentDesktopIndex == -1)
+                currentDesktopIndex = getCurrentDesktopIndex();
+
+            if (activePrograms[currentDesktopIndex] != null && activePrograms[currentDesktopIndex] != IntPtr.Zero)
+            {
+                SetForegroundWindow(activePrograms[currentDesktopIndex]);
+            }
+        }
+
+        private void changeTrayIcon(int currentDesktopIndex = -1)
+        {
+            if(currentDesktopIndex == -1) 
+                currentDesktopIndex = getCurrentDesktopIndex();
+
+            if(currentDesktopIndex == 0)
+                notifyIcon1.Icon = Properties.Resources._1;
+            else if (currentDesktopIndex == 1)
+                notifyIcon1.Icon = Properties.Resources._2;
+            else if (currentDesktopIndex == 2)
+                notifyIcon1.Icon = Properties.Resources._3;
+            else if (currentDesktopIndex == 3)
+                notifyIcon1.Icon = Properties.Resources._4;
+            else if (currentDesktopIndex == 4)
+                notifyIcon1.Icon = Properties.Resources._5;
+            else if (currentDesktopIndex == 5)
+                notifyIcon1.Icon = Properties.Resources._6;
+            else if (currentDesktopIndex == 6)
+                notifyIcon1.Icon = Properties.Resources._7;
+            else if (currentDesktopIndex == 7)
+                notifyIcon1.Icon = Properties.Resources._8;
+            else if (currentDesktopIndex == 8)
+                notifyIcon1.Icon = Properties.Resources._9;
+        }
+
+        VirtualDesktop initialDesktopState()
+        {
+            var desktop = VirtualDesktop.Current;
+            int desktopIndex = getCurrentDesktopIndex();
+
+            saveApplicationFocus(desktopIndex);
+
+            return desktop;
+        }
+
+        void RightKeyManagerPressed(object sender, KeyPressedEventArgs e)
+        {
+            var desktop = initialDesktopState();
+            
+            if(desktop.GetRight() != null)
+            {
+                desktop.GetRight()?.Switch();
+            } else
+            {
+                desktops.First()?.Switch();
+            }
+        }
+
+        void LeftKeyManagerPressed(object sender, KeyPressedEventArgs e)
+        {
+            var desktop = initialDesktopState();
+
+            if (desktop.GetLeft() != null)
+            {
+                desktop.GetLeft()?.Switch();
+            }
+            else
+            {
+                desktops.Last()?.Switch();
+            }
+        }
+    }
+}
